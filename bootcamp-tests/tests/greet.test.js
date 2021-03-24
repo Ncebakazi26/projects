2a63a46ae931b145204c346917618e9d35ceb9a9
Ncebakazi <ncebakazi414@gmail.com>
2021-03-24 16:54:04 +0200

    added new changes

---
+++ bootcamp-tests/tests/greet.test.js
@@ -0,0 +1,8 @@
+describe('The greet function',function(){
+    it('should greet Bob correctly',function(){
+  assert.equal('Hello, Bob',greet('Bob'))
+});
+it('Should greet Ncesh correctly',function(){
+assert.equal('Hello, Ncesh',greet('Ncesh'))
+});
+});
---
+++ bootcamp-tests/tests/test.js
@@ -0,0 +1,6 @@
+describe('this test' , function(){
+    it('should show you how to use MochaJS' , function(){
+        assert.equal(hello,2);
+    });
+
+});
